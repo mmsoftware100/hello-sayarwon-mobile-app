3e6655e6b198f13f674819a156a81aab91e0804a
Aung Ko Man <aungkoman@gmail.com>
2024-05-08 16:25:11 +0630

    Create article_repository.dart

---
+++ lib/hellosayarwon/domain/repositories/article_repository.dart
@@ -0,0 +1,11 @@
+
+abstract class ArticleRepository{
+  // CRUD
+  Future<Either<Failure,List<Article>>> getArticles({required GetArticlesPara getArticlesPara});
+  Future<Either<Failure, Article >> getArticle({required GetArticlePara getArticlePara});
+  Future<Either<Failure, Article >> updateArticle({required UpdateArticlePara updateArticlePara});
+
+  Future<Either<Failure,List<Category>>> getCategories({required GetCategoriesPara getCategoriesPara`});
+  Future<Either<Failure, Category >> getCategory({required GetCategoryPara getCategoryPara});
+
+}
