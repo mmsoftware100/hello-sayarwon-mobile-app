00db422815742f161edd8d3c1ab0b5d2f4ad681c
Aung Ko Man <aungkoman@gmail.com>
2024-05-10 13:26:48 +0630

    remote datasources implemented

--- lib/hellosayarwon/data/datasources/article_remote_datasource.dart
+++ lib/hellosayarwon/data/datasources/article_remote_datasource.dart
@@ -9,3 +9,24 @@ abstract class ArticleRemoteDatasource{
   Future< Article > getArticle({required GetArticlePara getArticlePara});
   Future< Article > updateArticle({required UpdateArticlePara updateArticlePara}); // for favourite
 }
+
+class ArticleRemoteDatasourceImpl implements ArticleRemoteDatasource{
+  @override
+  Future<Article> getArticle({required GetArticlePara getArticlePara}) {
+    // TODO: implement getArticle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Article>> getArticles({required GetArticlesPara getArticlesPara}) {
+    // TODO: implement getArticles
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Article> updateArticle({required UpdateArticlePara updateArticlePara}) {
+    // TODO: implement updateArticle
+    throw UnimplementedError();
+  }
+  
+}
--- lib/hellosayarwon/data/datasources/category_remote_datasource.dart
+++ lib/hellosayarwon/data/datasources/category_remote_datasource.dart
@@ -2,7 +2,22 @@ import '../../domain/entities/category.dart';
 import '../../domain/entities/paras/get_categories_para.dart';
 import '../../domain/entities/paras/get_category_para.dart';
 
-abstract class CategoryRepository{
-  Future< List<Category>>  getCategories({required GetCategoriesPara getCategoriesPara});
-  Future<  Category >  getCategory({required GetCategoryPara getCategoryPara});
+abstract class CategoryRemoteDatasource {
+  Future<List<Category>> getCategories( {required GetCategoriesPara getCategoriesPara});
+  Future<Category> getCategory({required GetCategoryPara getCategoryPara});
+}
+
+class CategoryRemoteDatasourceImpl implements CategoryRemoteDatasource{
+  @override
+  Future<List<Category>> getCategories({required GetCategoriesPara getCategoriesPara}) {
+    // TODO: implement getCategories
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Category> getCategory({required GetCategoryPara getCategoryPara}) {
+    // TODO: implement getCategory
+    throw UnimplementedError();
+  }
+  
 }
