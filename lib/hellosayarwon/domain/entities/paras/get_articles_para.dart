2e6b94217c2d80b8c9dd1cc333d433a0eeb5de18
Aung Ko Man <aungkoman@gmail.com>
2024-05-10 10:34:14 +0630

    test page added

--- lib/hellosayarwon/domain/entities/paras/get_articles_para.dart
+++ lib/hellosayarwon/domain/entities/paras/get_articles_para.dart
@@ -1,8 +1,14 @@
 
 class GetArticlesPara{
   final String accessToken;
+  final String query;
+  final int categoryId;
+  final int page;
 
   GetArticlesPara({
-    required this.accessToken
+    required this.accessToken,
+    required this.query,
+    required this.categoryId,
+    required this.page
   });
 }
--- lib/hellosayarwon/presentation/pages/test/test_page.dart
+++ lib/hellosayarwon/presentation/pages/test/test_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hellosayarwon/hellosayarwon/domain/entities/paras/get_articles_para.dart';
+import 'package:hellosayarwon/hellosayarwon/presentation/providers/article_provider.dart';
+import 'package:provider/provider.dart';
 
 class TestPage extends StatefulWidget {
   static const String routeName = "/TestPage";
@@ -11,6 +14,34 @@ class TestPage extends StatefulWidget {
 class _TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test Page"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(title: const Text("Refresh Articles"), onTap: _refreshArticles, ),
+          ListTile(title: const Text("Load More Articles"), onTap: _loadMoreArticles, ),
+          ListTile(title: const Text("Article Detail"), onTap: _articleDetail, ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _refreshArticles() async{
+    print("TestPage->_refreshArticles");
+    String accessToken = "";
+    String query = "";
+    int categoryId = 0;
+    int page = 1;
+    GetArticlesPara getArticlesPara = GetArticlesPara(accessToken: accessToken, query: query, categoryId: categoryId, page: page);
+    bool status = await Provider.of<ArticleProvider>(context, listen: false).getArticlesPlz(getArticlesPara: getArticlesPara);
+    print("TestPage->_refreshArticles status $status");
+  }
+  Future<void> _loadMoreArticles() async{
+
+  }
+  Future<void> _articleDetail() async{
+
   }
 }
